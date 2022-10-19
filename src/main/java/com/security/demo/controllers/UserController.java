d63c9f062215d2cdba8f08289df9e1ac7604ea23
Otabek <bekdevs01@gmail.com>
2022-10-19 16:53:19 +0900

    init

--- src/main/java/com/security/demo/controllers/UserController.java
+++ src/main/java/com/security/demo/controllers/UserController.java
@@ -1,11 +1,10 @@
-package com.security.demo;
+package com.security.demo.controllers;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.security.demo.models.User;
